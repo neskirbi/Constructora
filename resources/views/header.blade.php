eacb2da244ae12acf06a8360e1ab6576586d9a54
Raul <neskirbi@gmail.com>
2026-01-22 22:53:27 -0600

    listo el apartado de contratos

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+     <!-- Favicon con FontAwesome -->
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+        <text y=%22.9em%22 font-size=%2290%22>
+            <tspan fill=%22%23FFC107%22>⛑</tspan>
+        </text>
+    </svg>" type="image/svg+xml">
+    
     
     <!-- Disenios -->
     <link href="{{asset('css/plantilla.css')}}?ver={{Version()}}" rel="stylesheet"> 
