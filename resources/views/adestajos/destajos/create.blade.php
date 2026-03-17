65d93fd5cb1da65c69fd9122fdaf021b8c741521
Raul <neskirbi@gmail.com>
2026-03-17 13:50:52 -0600

    Cambio amortizacion

--- resources/views/adestajos/destajos/create.blade.php
+++ resources/views/adestajos/destajos/create.blade.php
@@ -172,7 +172,7 @@
                                                 <option value="">Seleccione un contrato</option>
                                                 @foreach($contratos as $contrato)
                                                     <option value="{{ $contrato->id }}" {{ old('id_contrato') == $contrato->id ? 'selected' : '' }}>
-                                                        {{ $contrato->consecutivo }} - {{ $contrato->obra ?? '' }}
+                                                        {{ $contrato->consecutivo }} - {{ $contrato->refinterna ?? '' }}
                                                     </option>
                                                 @endforeach
                                             </select>
